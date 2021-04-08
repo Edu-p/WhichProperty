a6c59dd3ae9521655d08e7103e1aa76276139b96
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-04-08 05:11:09 -0300

    filtering data, casting, sorting dataset

---
+++ Analysis.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+#------------
+#Extract
+#------------
+
+portifolio = pd.read_csv( 'dataset/kc_house_data.csv' )
+
+# print(portifolio.dtypes)
+
+# filtering data
+
+portWithFilter = portifolio[['id','price','zipcode','date','condition','yr_built','yr_renovated','bedrooms']]
+
+print(portWithFilter.shape)
+
+# removing outliers
+
+portWithFilter = portWithFilter.loc[(portWithFilter['price']<2000000) & (portWithFilter['bedrooms']<5)]
+
+print(portWithFilter.shape)
+
+# casting
+
+portWithFilter['date'] = pd.to_datetime(portWithFilter['date'])
+
+# sorting by date
+portWithFilter = portWithFilter.sort_values('date')
+
+print(portWithFilter['date'].head)
+
+#-------------
+#Trasform
+#-------------
+
+
+
