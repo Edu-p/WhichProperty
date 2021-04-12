f901390c8c230677e9ed9de009ddaa40a791a963
Edu-p <eduardoaamorim0@gmail.com>
2021-04-12 07:12:48 -0300

    feat: dataset with first sugest for CEO

--- Analysis.py
+++ Analysis.py
@@ -3,19 +3,14 @@ import pandas as pd
 
 portifolio = pd.read_csv( 'dataset/kc_house_data.csv' )
 
-# print(portifolio.dtypes)
-
 # filtering data
 
 portWithFilter = portifolio[['id','price','zipcode','date','condition','yr_built','yr_renovated','bedrooms']]
 
-# print(portWithFilter.shape)
 
 # removing outliers
 
 portWithFilter = portWithFilter.loc[(portWithFilter['price']<2000000) & (portWithFilter['bedrooms']<5)]
-
-# print(portWithFilter.shape)
 
 # casting
 
@@ -25,14 +20,30 @@ portWithFilter['date'] = pd.to_datetime(portWithFilter['date'])
 
 portWithFilter = portWithFilter.sort_values('date')
 
-# print(portWithFilter['date'].head)
-
 # grouping by zipcode
 
-portWithFilter[['price','zipcode']].groupby('zipcode').median()
+medianbyZipCode = portWithFilter[['price','zipcode']].groupby('zipcode').median().reset_index()
+
+# comparing properties by median of zipcode
+
+portWithFilter['median_price'] = 0
+portWithFilter['status'] = 'NA'
+
+# print(portWithFilter.columns)
+
+for i in range( len(portWithFilter) ):
+    for j in range( len(medianbyZipCode) ):
+        print()
+        if ( portWithFilter.iloc[i, 2] == medianbyZipCode.loc[j, 'zipcode'] ):
+            portWithFilter.iloc[i, 8] = medianbyZipCode.loc[j,'price']
+            if( portWithFilter.iloc[i, 1] > medianbyZipCode.loc[j, 'price'] ):
+                portWithFilter.iloc[i, 9] = 'not buy'
+            else:
+                portWithFilter.iloc[i, 9]  = 'buy'
+            continue
 
 
-
+print(portWithFilter.head(4))
 
 
 
