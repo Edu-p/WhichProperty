7b53861d3ac5e24ef6b692fbcddd829664fbcddd
Edu-p <eduardoaamorim0@gmail.com>
2021-04-19 07:13:37 -0300

    putting in probably final organization

--- Analysis.py
+++ Analysis.py
@@ -1,45 +1,79 @@
+
+
+# Imports
+
 import pandas as pd
 
+# Helper functions
+
+# Loading data
 
 portifolio = pd.read_csv( 'dataset/kc_house_data.csv' )
+portifolio = portifolio[['id', 'price', 'zipcode', 'date', 'condition', 'yr_built', 'yr_renovated', 'bedrooms']]
 
-# filtering data
 
-portWithFilter = portifolio[['id','price','zipcode','date','condition','yr_built','yr_renovated','bedrooms']]
+# Data Description
+
+
+
+
+## Data Dimensions
+
+## Data Types
+
+## Check NA
+
+## Fillout NA
+
+## Change types
+
+## Descriptive Statistical
+
+
+
+
+
+# Data Exp
+
+
+
+# Putting on Heroku
+
+
 
 
 # removing outliers
 
-portWithFilter = portWithFilter.loc[(portWithFilter['price']<2000000) & (portWithFilter['bedrooms']<5)]
+portifolio = portifolio.loc[(portifolio['price'] < 2000000) & (portifolio['bedrooms'] < 5)]
 
 # casting
 
-portWithFilter['date'] = pd.to_datetime(portWithFilter['date'])
+portifolio['date'] = pd.to_datetime(portifolio['date'])
 
 # sorting by date
 
-portWithFilter = portWithFilter.sort_values('date')
+portifolio = portifolio.sort_values('date')
 
 # grouping by zipcode
 
-medianbyZipCode = portWithFilter[['price','zipcode']].groupby('zipcode').median().reset_index()
+medianbyZipCode = portifolio[['price', 'zipcode']].groupby('zipcode').median().reset_index()
 
 # comparing properties by median of zipcode
 
-portWithFilter['median_price'] = 0
-portWithFilter['status'] = 'NA'
+portifolio['median_price'] = 0
+portifolio['status'] = 'NA'
 
 # print(portWithFilter.columns)
 
 
-for i in range( len(portWithFilter) ):
+for i in range(len(portifolio)):
     for j in range( len(medianbyZipCode) ):
-        if ( portWithFilter.iloc[i, 2] == medianbyZipCode.loc[j, 'zipcode'] ):
-            portWithFilter.iloc[i, 8] = medianbyZipCode.loc[j,'price']
-            if( portWithFilter.iloc[i, 1] > medianbyZipCode.loc[j, 'price'] ):
-                portWithFilter.iloc[i, 9] = 'not buy'
+        if ( portifolio.iloc[i, 2] == medianbyZipCode.loc[j, 'zipcode']):
+            portifolio.iloc[i, 8] = medianbyZipCode.loc[j, 'price']
+            if( portifolio.iloc[i, 1] > medianbyZipCode.loc[j, 'price']):
+                portifolio.iloc[i, 9] = 'not buy'
             else:
-                portWithFilter.iloc[i, 9]  = 'buy'
+                portifolio.iloc[i, 9]  = 'buy'
             continue
 
 
@@ -47,29 +81,29 @@ for i in range( len(portWithFilter) ):
 
 # when to buy the chosen properties, when to sell
 
-portWithFilter['season'] = 'StdSeason'
-portWithFilter['sell_price'] = 0
-portWithFilter['profit'] = 0
+portifolio['season'] = 'StdSeason'
+portifolio['sell_price'] = 0
+portifolio['profit'] = 0
 
 
-portWithFilter.loc[ (portWithFilter['date'].dt.month >= 3) & (portWithFilter['date'].dt.month < 9), 'season'] = 'Summer'
-portWithFilter.loc[ (portWithFilter['date'].dt.month >= 9) | (portWithFilter['date'].dt.month < 3), 'season'] = 'Winter'
+portifolio.loc[(portifolio['date'].dt.month >= 3) & (portifolio['date'].dt.month < 9), 'season'] = 'Summer'
+portifolio.loc[(portifolio['date'].dt.month >= 9) | (portifolio['date'].dt.month < 3), 'season'] = 'Winter'
 
-medianbyZipCodeandSeason = portWithFilter[['price','zipcode','season']].groupby( ['zipcode','season'] ).median().reset_index()
+medianbyZipCodeandSeason = portifolio[['price', 'zipcode', 'season']].groupby(['zipcode', 'season']).median().reset_index()
 
-for i in range ( len(portWithFilter) ):
-    if portWithFilter.iloc[i, 9] == 'buy':
+for i in range (len(portifolio)):
+    if portifolio.iloc[i, 9] == 'buy':
         for j in range( len(medianbyZipCodeandSeason) ):
-            if(portWithFilter.iloc[i, 2] == medianbyZipCodeandSeason.loc[j, 'zipcode']) & ( medianbyZipCodeandSeason.loc[j, 'season'] == 'Summer' ):
-                if( portWithFilter.iloc[i,1] >= medianbyZipCodeandSeason.loc[j,'price'] ):
-                    portWithFilter.iloc[i, 11]  = portWithFilter.iloc[i,1]*1.1
+            if(portifolio.iloc[i, 2] == medianbyZipCodeandSeason.loc[j, 'zipcode']) & (medianbyZipCodeandSeason.loc[j, 'season'] == 'Summer'):
+                if( portifolio.iloc[i, 1] >= medianbyZipCodeandSeason.loc[j, 'price']):
+                    portifolio.iloc[i, 11]  = portifolio.iloc[i, 1] * 1.1
                     break
                 else:
-                    portWithFilter.iloc[i, 11] = portWithFilter.iloc[i, 1] * 1.3
+                    portifolio.iloc[i, 11] = portifolio.iloc[i, 1] * 1.3
                     break
 
-    portWithFilter.iloc[i,12] = portWithFilter.iloc[i,11] - portWithFilter.iloc[i,1]
+    portifolio.iloc[i, 12] = portifolio.iloc[i, 11] - portifolio.iloc[i, 1]
 
-print(portWithFilter[['status','price','zipcode','season','sell_price','profit']].head(50))
+print(portifolio[['status', 'price', 'zipcode', 'season', 'sell_price', 'profit']].head(50))
 
-print( 'Total Profit: {}'.format( portWithFilter['profit'][portWithFilter['profit'] > 0].sum() ) )
+print( 'Total Profit: {}'.format(portifolio['profit'][portifolio['profit'] > 0].sum()))
