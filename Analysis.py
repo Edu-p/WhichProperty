ec3bed1d8ad71f80f4e68920b558b5bf13dbe3c6
Edu-p <eduardoaamorim0@gmail.com>
2021-04-19 07:31:12 -0300

    descriptive analysis of numerical attributes

--- Analysis.py
+++ Analysis.py
@@ -3,6 +3,7 @@
 # 1.0 Imports
 
 import pandas as pd
+import numpy as np
 
 # 2.0 Helper functions
 
@@ -23,13 +24,11 @@ print( 'number of Rows: {}'.format( portifolio.shape[1] ) )
 ## 4.0.2 Data Types
 
 portifolio['date'] = pd.to_datetime(portifolio['date'])
-portifolio.dtypes
+print( portifolio.dtypes )
 
 ## 4.0.3 Check NA
 
-portifolio.isna().sum()
-
-## 4.0.4 Fillout NA
+print(portifolio.isna().sum())
 
 ## 4.0.5 Removing Outliers
 
@@ -42,9 +41,41 @@ portifolio['date'] = pd.to_datetime(portifolio['date'])
 
 ## 4.0.7 Descriptive Statistical
 
+### 4.0.7.1 Numerical Attributes
+
+# Central Tendency -  mean, median
+ct1 = pd.DataFrame( num_attributes.apply( np.mean ) ).T
+ct2 = pd.DataFrame( num_attributes.apply( np.median ) ).T
+
+# Dispersion - std, min, max, range, skev, kurtosis
+d1 = pd.DataFrame( num_attributes.apply( np.std ) ).T # Esse T é para uma vermos melhor o dataset
+d2 = pd.DataFrame( num_attributes.apply( min ) ).T # Esse T é para uma vermos melhor o dataset
+d3 = pd.DataFrame( num_attributes.apply( max ) ).T # Esse T é para uma vermos melhor o dataset
+d4 = pd.DataFrame( num_attributes.apply( lambda x: x.max() - x.min() ) ).T # Esse T é para uma vermos melhor o dataset
+d5 = pd.DataFrame( num_attributes.apply( lambda x: x.skew() ) ).T # Esse T é para uma vermos melhor o dataset
+d6 = pd.DataFrame( num_attributes.apply( lambda x: x.kurtosis ) ).T # Esse T é para uma vermos melhor o dataset
+
+# concatenate
+dfDesc = pd.concat( [ d2, d3, d4, ct1, ct2, d1, d5, d4 ] ).T.reset_index()
+
+dfDesc.columns = ['attributes', 'min', 'max', 'range', 'mean', 'median', 'std', 'skew', 'kurtosis']
+
+
+### 4.0.7.2 Categorical Attributes
 
 
 
+
+
+
+
+## 4.0.8 Feature engeneering
+
+portifolio['median_price'] = 0
+portifolio['status'] = 'NA'
+portifolio['season'] = 'StdSeason'
+portifolio['sell_price'] = 0
+portifolio['profit'] = 0
 
 # 5.0 Data Exp
 
@@ -53,58 +84,3 @@ portifolio['date'] = pd.to_datetime(portifolio['date'])
 # 6.0 Putting on Heroku
 
 
-
-
-# grouping by zipcode
-
-medianbyZipCode = portifolio[['price', 'zipcode']].groupby('zipcode').median().reset_index()
-
-# comparing properties by median of zipcode
-
-portifolio['median_price'] = 0
-portifolio['status'] = 'NA'
-
-# print(portWithFilter.columns)
-
-
-for i in range(len(portifolio)):
-    for j in range( len(medianbyZipCode) ):
-        if ( portifolio.iloc[i, 2] == medianbyZipCode.loc[j, 'zipcode']):
-            portifolio.iloc[i, 8] = medianbyZipCode.loc[j, 'price']
-            if( portifolio.iloc[i, 1] > medianbyZipCode.loc[j, 'price']):
-                portifolio.iloc[i, 9] = 'not buy'
-            else:
-                portifolio.iloc[i, 9]  = 'buy'
-            continue
-
-
-# print(portWithFilter.head(15))
-
-# when to buy the chosen properties, when to sell
-
-portifolio['season'] = 'StdSeason'
-portifolio['sell_price'] = 0
-portifolio['profit'] = 0
-
-
-portifolio.loc[(portifolio['date'].dt.month >= 3) & (portifolio['date'].dt.month < 9), 'season'] = 'Summer'
-portifolio.loc[(portifolio['date'].dt.month >= 9) | (portifolio['date'].dt.month < 3), 'season'] = 'Winter'
-
-medianbyZipCodeandSeason = portifolio[['price', 'zipcode', 'season']].groupby(['zipcode', 'season']).median().reset_index()
-
-for i in range (len(portifolio)):
-    if portifolio.iloc[i, 9] == 'buy':
-        for j in range( len(medianbyZipCodeandSeason) ):
-            if(portifolio.iloc[i, 2] == medianbyZipCodeandSeason.loc[j, 'zipcode']) & (medianbyZipCodeandSeason.loc[j, 'season'] == 'Summer'):
-                if( portifolio.iloc[i, 1] >= medianbyZipCodeandSeason.loc[j, 'price']):
-                    portifolio.iloc[i, 11]  = portifolio.iloc[i, 1] * 1.1
-                    break
-                else:
-                    portifolio.iloc[i, 11] = portifolio.iloc[i, 1] * 1.3
-                    break
-
-    portifolio.iloc[i, 12] = portifolio.iloc[i, 11] - portifolio.iloc[i, 1]
-
-print(portifolio[['status', 'price', 'zipcode', 'season', 'sell_price', 'profit']].head(50))
-
-print( 'Total Profit: {}'.format(portifolio['profit'][portifolio['profit'] > 0].sum()))
