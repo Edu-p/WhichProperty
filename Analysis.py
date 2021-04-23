84dc0b390688c53fb1344f35dffb3a04adff9821
Edu-p <eduardoaamorim0@gmail.com>
2021-04-23 06:37:00 -0300

    putting on functions

--- Analysis.py
+++ Analysis.py
@@ -10,69 +10,116 @@ from IPython.display import Image
 
 # 2.0 Helper functions
 
+def loading_data( path ):
+
+    data = pd.read_csv(path)
+
+    return data
+
+def filtering_data( data ):
+
+    data = data[['id', 'price', 'zipcode', 'date', 'condition', 'yr_built', 'yr_renovated', 'bedrooms']]
+
+    return data
+
+def show_data_dimensions( data ):
+
+    print('number of Rows: {}'.format(portifolio.shape[0]))
+    print('number of Rows: {}'.format(portifolio.shape[1]))
+    print('----------------------------\n')
+
+    return None
+
+def show_data_types( data ):
+
+    data['date'] = pd.to_datetime(data['date'])
+    print(portifolio.dtypes)
+
+    return data
+
+def checkNa( data ):
+    print(portifolio.isna().sum())
+    print('----------------------------\n')
+    return None
+
+def removing_outliers( data ):
+
+    data = data.loc[(data['price'] < 2000000) & (data['bedrooms'] < 5)]
+
+    return data
+
+def change_types( data ):
+
+    data['date'] = pd.to_datetime(data['date'])
+
+    return data
+
+def descriptive_statistical( data ):
+
+    pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+    ### 4.0.7.1 Numerical Attributes
+
+    num_attributes = portifolio.select_dtypes(include=['int64', 'float64'])
+    cat_attributes = portifolio.select_dtypes(exclude=['int64', 'float64', 'datetime64[ns]'])
+
+    #### 4.0.7.1.1 Central Tendency -  mean, median
+    ct1 = pd.DataFrame(num_attributes.apply(np.mean)).T
+    ct2 = pd.DataFrame(num_attributes.apply(np.median)).T
+
+    #### 4.0.7.1.2 Dispersion - std, min, max, range, skev, kurtosis
+    d1 = pd.DataFrame(num_attributes.apply(np.std)).T
+    d2 = pd.DataFrame(num_attributes.apply(min)).T
+    d3 = pd.DataFrame(num_attributes.apply(max)).T
+    d4 = pd.DataFrame(num_attributes.apply(lambda x: x.max() - x.min())).T
+    d5 = pd.DataFrame(num_attributes.apply(lambda x: x.skew())).T
+    d6 = pd.DataFrame(num_attributes.apply(lambda x: x.kurtosis)).T
+
+    dfDesc = pd.concat([d2, d3, d4, ct1, ct2, d1, d5, d4]).T.reset_index()
+
+    dfDesc.columns = ['attributes', 'min', 'max', 'range', 'mean', 'median', 'std', 'skew', 'kurtosis']
+
+    print(dfDesc)
+    print('----------------------------\n')
+
+    return None
 
 
 # 3.0 Loading data(*)
 
-portifolio = pd.read_csv( 'dataset/kc_house_data.csv' )
+portifolio = loading_data( 'dataset/kc_house_data.csv' )
 
 ### 3.0.1 filtering data
-portifolio = portifolio[['id', 'price', 'zipcode', 'date', 'condition', 'yr_built', 'yr_renovated', 'bedrooms']]
+
+portifolio = filtering_data( portifolio )
 
 
 # 4.0 Data Description
 
 ## 4.0.1 Data Dimensions
 
-print( 'number of Rows: {}'.format( portifolio.shape[0] ) )
-print( 'number of Rows: {}'.format( portifolio.shape[1] ) )
-print('----------------------------\n')
+show_data_dimensions(portifolio)
+
 ## 4.0.2 Data Types
 
-portifolio['date'] = pd.to_datetime(portifolio['date'])
-print( portifolio.dtypes )
+portifolio = show_data_types( portifolio )
 
 ## 4.0.3 Check NA
 
-print(portifolio.isna().sum())
-print('----------------------------\n')
+checkNa(portifolio)
 
 ## 4.0.5 Removing Outliers
 
-portifolio = portifolio.loc[(portifolio['price'] < 2000000) & (portifolio['bedrooms'] < 5)]
+portifolio = removing_outliers( portifolio )
 
 
 ## 4.0.6 Change types
 
-portifolio['date'] = pd.to_datetime(portifolio['date'])
+portifolio = change_types( portifolio )
 
 ## 4.0.7 Descriptive Statistical
 
-pd.set_option('display.float_format', lambda x: '%.2f' % x)
-
-### 4.0.7.1 Numerical Attributes
-
-num_attributes = portifolio.select_dtypes( include=['int64', 'float64'] )
-cat_attributes = portifolio.select_dtypes( exclude=['int64', 'float64', 'datetime64[ns]'] )
-
-#### 4.0.7.1.1 Central Tendency -  mean, median
-ct1 = pd.DataFrame( num_attributes.apply( np.mean ) ).T
-ct2 = pd.DataFrame( num_attributes.apply( np.median ) ).T
-
-#### 4.0.7.1.2 Dispersion - std, min, max, range, skev, kurtosis
-d1 = pd.DataFrame( num_attributes.apply( np.std ) ).T
-d2 = pd.DataFrame( num_attributes.apply( min ) ).T
-d3 = pd.DataFrame( num_attributes.apply( max ) ).T
-d4 = pd.DataFrame( num_attributes.apply( lambda x: x.max() - x.min() ) ).T
-d5 = pd.DataFrame( num_attributes.apply( lambda x: x.skew() ) ).T
-d6 = pd.DataFrame( num_attributes.apply( lambda x: x.kurtosis ) ).T
-
-dfDesc = pd.concat( [ d2, d3, d4, ct1, ct2, d1, d5, d4 ] ).T.reset_index()
-
-dfDesc.columns = ['attributes', 'min', 'max', 'range', 'mean', 'median', 'std', 'skew', 'kurtosis']
-
-print(dfDesc)
-print('----------------------------\n')
+descriptive_statistical(portifolio)
 
 ## 4.0.8 Feature engeneering
 
@@ -83,7 +130,8 @@ print('----------------------------\n')
     # 4. are older properties cheaper?
     # 5. are non-renovated properties cheaper?
 
-### 4.0.8.2 crating columns
+### 4.0.8.2 creating columns
+
 portifolio['profit'] = 0
 portifolio['status'] = 'NA'
 portifolio['sell_price'] = 0
@@ -105,7 +153,6 @@ for i in range(len(portifolio)):
         if ( portifolio.iloc[i, 2] == medianbyZipCode.loc[j, 'zipcode']):
             print(i,j)
             portifolio.iloc[i, 11] = medianbyZipCode.loc[j, 'price']
-
             continue
 
 
